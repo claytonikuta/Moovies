3921b2f9750372225306f6ec782273f05d224a0f
cikuta <clayton.ikuta@gmail.com>
2024-02-22 00:13:09 -0800

    declare global in mongodb

--- lib/db/mongodb.ts
+++ lib/db/mongodb.ts
@@ -1,6 +1,14 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from 'mongodb';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      _mongoClientPromise: Promise<MongoClient>;
+    }
+  }
+}
+
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
